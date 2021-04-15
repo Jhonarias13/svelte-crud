48b7de9bc686d4b8154837cdc861c365056a86f1
Jhon Freiman Arias <jhonarias1322@gmail.com>
2021-04-15 07:43:39 -0500

    termiando el pryecto, arreglando toastr

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { db } from "./firebase";
+  import toastr from "toastr";
 
   let task = {
     name: "",
@@ -27,7 +28,7 @@
         ...task,
         createdAt: Date.now(),
       });
-    alert("new task created");
+    toastr.success("New Task created");
   };
 
   const updateTask = async () => {
@@ -67,46 +68,51 @@
 </script>
 
 <main class="container h-auto">
-  <div>
+  <div class="header">
     <h1 class="text-center m-2">TaskAPP</h1>
   </div>
   <form on:submit|preventDefault={handleSubmit} class="card card-body">
     <div class="form-group">
       <input
-      bind:value={task.name}
-      type="text"
-      placeholder="write a new task"
-      class="form-control"
-    />
+        bind:value={task.name}
+        type="text"
+        placeholder="write a new task"
+        class="form-control"
+      />
     </div>
     <div class="form-group">
       <textarea
-      bind:value={task.description}
-      type="text"
-      placeholder="write a  task description"
-      rows="3"
-      class="form-control"
-    />
+        bind:value={task.description}
+        type="text"
+        placeholder="write a  task description"
+        rows="3"
+        class="form-control"
+      />
     </div>
+
     <button class="btn btn-primary">
-      {#if !editStatus}Save {:else}Update{/if}
+      {#if !editStatus}<i class="far fa-save fa-lg" />
+      {:else}<i class="fas fa-check fa-lg mr-2" />Update{/if}
     </button>
     {#if editStatus}
-      <button on:click={onCancel} class="btn btn-info mt-2">cancel</button>
+      <button on:click={onCancel} class="btn btn-info mt-2">
+        <i class="far fa-window-close fa-lg mr-2" />Cancel
+      </button>
     {/if}
   </form>
 
   {#each tasks as task}
     <div class="card card-body mt-4">
-      <h5>{task.name}</h5>
+      <div class="d-flex justify-content-between align-items-center">
+        <h5>{task.name}</h5>
+        <i class="fas fa-pen fa-lg" on:click={editTask(task)} />
+      </div>
       <p>{task.description}</p>
       <button on:click={deleteTask(task.id)} class="btn btn-danger"
-        >Delete</button
+        ><i class="far fa-trash-alt fa-lg" /></button
       >
-      <button on:click={editTask(task)} class="btn btn-info mt-2">Edit</button>
     </div>
   {/each}
-  
 </main>
 <footer class="footer p-3 mt-4 text-center bg-light">
   Development By <a href="http://www.studio.dev">StudioDev</a>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import App from './App.svelte';
 import './firebase';
 
-import 'bootswatch/dist/cyborg/bootstrap.min.css';
+import 'bootswatch/dist/lux/bootstrap.min.css';
+import 'toastr/build/toastr.min.js';
+import 'toastr/build/toastr.min.css';
 
 const app = new App({
     target: document.body,
